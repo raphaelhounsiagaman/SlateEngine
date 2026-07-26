18357620b4ae6d7bc3aa324d6a4eaf4f00ec58fa
raphaelhounsiagaman <rhounsiagaman@gmail.com>
2026-07-26 15:10:02 +0200

    Added more info to cam struct

--- Slate/include/Slate/Graphics/Camera3D.h
+++ Slate/include/Slate/Graphics/Camera3D.h
@@ -6,8 +6,10 @@ namespace Slate
 {
 	struct Camera3D
 	{
-		Transform3D Transform;
-		float FOV;
+		Transform3D Transform{};
+		float FOV = 60.0f * 3.1415926535f / 180.0f;
+		float NearPlane = 0.1f;
+		float FarPlane = 1000.0f;
 	};
 }
 
